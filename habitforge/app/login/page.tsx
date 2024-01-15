c2964bdc628a540116c52419ddc3882d14a24d4c
a-patel02 <89111965+a-patel02@users.noreply.github.com>
2024-01-14 20:24:21 -0500

    Showcasing

--- habitforge/app/login/page.tsx
+++ habitforge/app/login/page.tsx
@@ -3,30 +3,28 @@ import { Button } from "@/components/ui/button";
 import Typography from "@/components/ui/typography-variants";
 import { UserAuth } from "@/app/context/AuthContext";
 import { FcGoogle } from "react-icons/fc";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, doc, setDoc } from "firebase/firestore";
 import { db } from "@/firebase/firebase";
-import { useEffect } from "react";
 
 const LoginPage = () => {
   const { user, signInWithGoogle } = UserAuth();
 
-  async function addDocToCollection() {
+  const addDocToCollection = () => {
     try {
-      const docRef = await addDoc(collection(db, "users"), {
+      setDoc(doc(db, "users", user.uid), {
         email: user.email,
-        uid: user.uid,
         displayName: user.displayName,
       });
-      console.log("Document written with ID: ", docRef.id);
+      console.log("Document written");
     } catch (error) {
       console.error("Error adding document: ", error);
     }
-  }
+  };
 
   const handleSignIn = async () => {
     try {
       await signInWithGoogle();
-      await addDocToCollection();
+      await addDocToCollection;
     } catch (error) {
       console.log(error);
     }
